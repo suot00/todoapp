6afc502dd72ff8d18923ef65030cbdfa2d774ca1
suot00 <truongkhacsuot661@gmail.com>
2022-07-10 20:48:22 +0700

    fix checked

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ var filters = document.querySelectorAll(".filterTask span")
 
 let localItems =JSON.parse(localStorage.getItem('localItem'))
 
+let isUpdated = false;
+let idUpdated =null;
+
 filters.forEach(btn=>{
   btn.addEventListener("click", () =>{ 
     document.querySelector("span.active").classList.remove("active");
@@ -20,22 +23,23 @@ function showTaskList(filter){
   let taskItem='';
   let taskListShow=document.querySelector('.listItem')
   let localItems =JSON.parse(localStorage.getItem('localItem'))
-  if(localItems ===null){
+  if(localItems ===null ){
    taskList=[]
   } else{
    taskList=localItems
   }  
   taskList.forEach((data,id)=>{
-    let isComplete = data.status === 'completed' ? 'checked' : '';
+    let completed = data.status === 'completed' ? 'checked' : '';
     if(filter == data.status || filter=="all"){
       taskItem+=`
       <ul class="taskItem">
           <li class="task">
               <label for="${id}" class="taskLabel">
-                  <input onClick="updateStatus(this)" type="checkbox" class="taskIp" id="${id}">
-                  <p class="nameTask ${isComplete}">${data.name}</p>
+                  <input type="checkbox" onClick="updateStatus(this)" ${completed}  id="${id}">
+                  <p class="nameTask ${completed} ">${data.name}</p>
              </label>       
           <div class="menu-task">
+              <button class="edit-task" onClick="EditItem(${id})">Edit</button>
               <button class="delete-task" onClick="deleteItem(${id})">Delete</button>
           </div>
           </li>
@@ -51,14 +55,13 @@ showTaskList("all")
 
 // console.log(localItems)
 addBtn.addEventListener('click', () =>{   
-  // let userText= inputText.value.trim();
+ 
+  
   if(inputText.value !=0 ){
     let localItems =JSON.parse(localStorage.getItem('localItem'))
-
     if(localItems ===null ){
      taskList=[]
     } else{
-     
      taskList=localItems
     }  
     let taskInfo={name:inputText.value , status:'pending'}
@@ -69,18 +72,32 @@ addBtn.addEventListener('click', () =>{
    showTaskList('all')
 })
 
+function EditItem(id){
+     let title ;
+     taskList.forEach((taskL,Id)=>{
+      if(Id ==id){
+        title = taskL.name
+      }
+     }) 
 
+     addBtn.innerText="Update";
 
+     inputText.value = title;
+     inputText.focus();
+
+     idUpdate = id;
+     isUpdate = true;
+}
 //delete item
 function deleteItem(id){
   taskList.splice(id,1)
   localStorage.setItem('localItem',JSON.stringify(taskList))
-  showTaskList()
+  showTaskList('all')
 }
 //clear all task
 function clearTask(){
   localStorage.clear()
-  showTaskList()
+  showTaskList('all')
 }
 //update Status
 function updateStatus(choiceTask) {
